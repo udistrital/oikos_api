de58f38589cec64af4542c10e6fd5a63b7e3fe0a
Javier Reyes <javierreyes@s112111c7g.udistritaloas.edu.co>
2017-03-27 17:02:10 -0500

    Se configura el cors y se ajusta la ruta de los fuentes del api.

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-	_ "github.com/udistrital/oikos/oikos_api/routers"
+	_ "github.com/udistrital/oikos_api/routers"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/lib/pq"
+	"github.com/astaxie/beego/plugins/cors"
 )
 
 func init() {
-	orm.RegisterDataBase("default", "postgres", "postgres://administrador_oikos:oikos@127.0.0.1/espacios_fisicos?sslmode=disable")
+	//orm.RegisterDataBase("default", "postgres", "postgres://administrador_oikos:oikos@127.0.0.1/espacios_fisicos?sslmode=disable")
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGhost")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
 }
 
 func main() {
+	//Configurando cors
+	orm.Debug = true
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+	AllowOrigins: []string{"*"},
+	AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+	AllowHeaders: []string{"Origin", "x-requested-with",
+	"content-type",
+	"accept",
+	"origin",
+	"authorization",
+	"x-csrftoken"},
+	ExposeHeaders: []string{"Content-Length"},
+	AllowCredentials: true,
+	}))
+
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
