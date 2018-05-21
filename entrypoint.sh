0134676ae87c43403c713ba37eed2bb789007905
Fabio Parra <faparraf@gmail.com>
2018-05-21 14:27:30 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export OIKOS_API_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/oikos_api/db/username --output text --query Parameter.Value)"
-  export OIKOS_API_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/oikos_api/db/password --output text --query Parameter.Value)"
+  export OIKOS_API_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/oikos_api/db/username --output text --query Parameter.Value)"
+  export OIKOS_API_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/oikos_api/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
