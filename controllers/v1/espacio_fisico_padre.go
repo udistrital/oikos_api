e263f55d59b20be3c0845aaf728c2db583c64f6f
Alex F. Bustos P <alexfbp@gmail.com>
2022-07-24 23:53:14 -0500

    fix: fields+sortby+query en v1/espacio_fisico_padre #39

--- controllers/v1/espacio_fisico_padre.go
+++ controllers/v1/espacio_fisico_padre.go
@@ -3,13 +3,17 @@ package controllers
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 
 	"github.com/udistrital/oikos_api/models"
+	"github.com/udistrital/utils_oas/formatdata"
 )
 
 // EspacioFisicoPadreController oprations for EspacioFisicoPadre
@@ -132,7 +136,11 @@ func (c *EspacioFisicoPadreController) GetAll() {
 		}
 	}
 
-	l, err := models.GetAllEspacioFisicoPadre(query, fields, sortby, order, offset, limit)
+	aux := models.EspacioFisicoPadreV2{}
+	l, err := models.GetAllEspacioFisicoPadre(aux.QueryFromV1(query),
+		aux.SelectorsFromV1(fields),
+		aux.SelectorsFromV1(sortby), order,
+		offset, limit)
 	if err != nil {
 		c.Data["json"] = err.Error()
 	} else {
@@ -141,7 +149,21 @@ func (c *EspacioFisicoPadreController) GetAll() {
 		for _, i := range l {
 			switch v := i.(type) {
 			case map[string]interface{}:
-				temp = append(temp, v)
+				// len(fields) > 0
+				var (
+					v2    models.EspacioFisicoPadreV2
+					v1aux models.EspacioFisicoPadre
+					v1    map[string]interface{}
+					err   error
+				)
+				formatdata.FillStruct(v, &v2)                     // convertir a estructura v2 ...
+				v2.ToV1(&v1aux)                                   // ... para poder convertir a v1
+				formatdata.FillStruct(v1aux, &v1)                 // Luego a un mapeo auxiliar a ser...
+				if v1, err = FilterKeys(v1, fields); err != nil { // ...filtrado
+					logs.Error(err)
+					c.Abort(fmt.Sprint(http.StatusInternalServerError))
+				}
+				temp = append(temp, v1)
 			case models.EspacioFisicoPadreV2:
 				var x models.EspacioFisicoPadre
 				v.ToV1(&x)
